c01c64e379c7e8f35f7d9ab23dff6c553e062abc
Roman Exempliarov <rexe@ya.ru>
2017-10-02 23:10:33 +0300

    [+] wip

--- kotolang-rb/lib/kotolang.rb
+++ kotolang-rb/lib/kotolang.rb
@@ -50,6 +50,18 @@ module BypassPipe
   end
 end
 
+module Str
+  module Eq
+    def self.call(input, config, pipes)
+      if input.is_a?(String) && config.is_a?(String)
+        ['ok', input == config]
+      else
+        ['error', nil]
+      end
+    end
+  end
+end
+
 module Arr
   module Get
     def self.call(input, config, pipes)
@@ -174,7 +186,12 @@ class Kotolang
       input_way, input = memo
       step_way, pipe, config = step
       if input_way == step_way
-        pipes[pipe].(input, config, pipes)
+        callable = pipes[pipe]
+        if callable.respond_to?(:call)
+          pipes[pipe].(input, config, pipes)
+        else
+          ['error', ['Function not found', "Unknown function #{pipe}"]]
+        end
       else
         memo # bypass
       end
@@ -184,6 +201,17 @@ end
 
 module Std
   module Way
+    module Call
+      def self.call(input, config, pipes)
+        way, result = ::Std::Call.(input, config, pipes)
+        if 'ok' == way
+          [result, input]
+        else
+          ['error', nil]
+        end
+      end
+    end
+
     def self.call(input, config, pipes)
       if config.is_a?(String)
         [config, input]
@@ -203,22 +231,129 @@ module Std
     end
   end
 
-  module WayFlow
+  module If
     def self.call(input, config, pipes)
-      way, result = Call.(input, config, pipes)
-      if 'ok' == way
-        [result, input]
+      expr_flow = config['flow']
+
+      expr_flow_result = Call.(
+        input,
+        expr_flow,
+        pipes
+      )
+
+      if expr_flow_result == ['ok', true]
+        then_flow = config['then']
+        if then_flow.nil?
+          ['ok', nil]
+        else
+          Call.(
+            input,
+            then_flow,
+            pipes
+          )
+        end
+      elsif expr_flow_result == ['ok', false]
+        else_flow = config['else']
+        if else_flow.nil?
+          ['ok', nil]
+        else
+          Call.(
+            input,
+            else_flow,
+            pipes
+          )
+        end
+      else
+        ['error', ['Flow result is not boolean']]
+      end
+    end
+  end
+
+  module Switch
+    def self.call(input, config, pipes)
+      expr_flow = config['flow']
+
+      expr_flow_result = Call.(
+        input,
+        expr_flow,
+        pipes
+      )
+
+      if expr_flow_result == ['ok', true]
+        then_flow = config['then']
+        if then_flow.nil?
+          ['ok', nil]
+        else
+          Call.(
+            input,
+            then_flow,
+            pipes
+          )
+        end
+      elsif expr_flow_result == ['ok', false]
+        else_flow = config['else']
+        if else_flow.nil?
+          ['ok', nil]
+        else
+          Call.(
+            input,
+            else_flow,
+            pipes
+          )
+        end
+      else
+        ['error', ['Flow result is not boolean']]
+      end
+    end
+  end
+  # module Switch
+  #   def self.call(input, config, pipes)
+  #     flow = config['flow']
+  #     return nil unless flow.is_a?(Array)
+
+  #     flow = Call.(
+  #       input,
+  #       config,
+  #       pipes
+  #     )
+  #   end
+  # end
+
+  module Type
+    def self.call(input, config, pipes)
+      if input.nil?
+        ['ok', 'null']
+      elsif input.is_a?(TrueClass) || input.is_a?(FalseClass)
+        ['ok', 'boolean']
+      elsif input.is_a?(Array)
+        ['ok', 'array']
+      elsif input.is_a?(Hash)
+        ['ok', 'object']
+      elsif input.is_a?(String)
+        ['ok', 'string']
+      elsif input.is_a?(Integer)
+        ['ok', 'integer']
+      elsif input.is_a?(Float)
+        ['ok', 'float']
       else
         ['error', nil]
       end
     end
   end
-
-  # module Switch
-  #   def self.call(input, config, pipes)
-  #     Call.(
-  #       input,
-  #     )
-  #   end
-  # end
 end
+
+# call, [
+#   ['ok', 'std:way:call', [
+#     ['ok', 'get', 'a'],
+#     ['ok', 'eq', '10'],
+#     ['ok', 'bool:to_int'],
+#     ['ok', 'arr:get', ['else', 'then']]
+#   ]]
+  
+# ]
+
+# 'switch', [
+#   'flow' => [['ok', 'get', 'a'], ['ok', 'eq', 10]],
+#   'then' => [['ok',]],
+#   'else' => []
+# ]
--- kotolang-rb/test/test_meme.rb
+++ kotolang-rb/test/test_meme.rb
@@ -6,23 +6,39 @@ require_relative "../lib/kotolang.rb"
 pipes = {
   'bypass' => BypassPipe,
   'mock' => MockPipe,
-  'obj:get' => Obj::Get,
-  'obj:set' => Obj::Set,
-  'arr:get' => Arr::Get,
-  'arr:set' => Arr::Set,
+  'std:obj:get' => Obj::Get,
+  'std:obj:set' => Obj::Set,
+  'std:arr:get' => Arr::Get,
+  'std:arr:set' => Arr::Set,
+  'std:str:eq' => Str::Eq,
   'std:way' => Std::Way,
   'std:call' => Std::Call,
-  'std:wayflow' => Std::WayFlow,
-  #'copy' => CopyPipe,
-  #'get' => GetPipe,
-  #'move' => MovePipe,
-  # 'set' => SetPipe
+  'std:way:call' => Std::Way::Call,
+  'std:if' => Std::If,
+  'std:type' => Std::Type,
+  'get' => -> (input, config, pipes) do
+    Kotolang.(['ok', input], [
+      ['ok', 'std:way:call', [
+        ['ok', 'std:type', nil]
+      ]],
+      ['array', 'std:arr:get', config],
+      ['object', 'std:obj:get', config]
+      # ['string', 'std:str:get', config],
+      # ['boolean', 'std::get', config],
+      # ['integer', 'std::get', config],
+      # ['float', 'std::get', config],
+      # ['null', 'std::get', config],
+    ], pipes)
+  end,
+  'debug' => -> (input, config) do
+    puts [input, config]
+  end
 }
 
 Dir.glob(
   File.join(
     File.dirname(__FILE__),
-    '..', '..', 'spec', '*.spec.json'
+    '..', '..', 'spec', '**', '*.spec.json'
   )
 ).each do |spec_file_name|
   f = File.read(spec_file_name)
